c13ed143bb493b9de1d20f4e04df3b1517c4c099
Zaid Ali <zaidali786908@gmail.com>
2025-06-17 13:01:26 +0500

    Make more views

---
+++ Islamia College Portfoio/Preview Content/Views/LibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryView.swift
+//  Islamia College Portfoio
+//
+//  Created by Development on 17/06/2025.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LibraryView()
+}
---
+++ Islamia College Portfoio/Preview Content/Views/ParkingView.swift
@@ -0,0 +1,18 @@
+//
+//  ParkingView.swift
+//  Islamia College Portfoio
+//
+//  Created by Development on 17/06/2025.
+//
+
+import SwiftUI
+
+struct ParkingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParkingView()
+}
