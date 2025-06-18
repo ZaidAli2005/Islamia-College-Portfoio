0f69352b14065c5acfe81604adba901a0d606051
Zaid Ali <zaidali786908@gmail.com>
2025-06-18 20:49:28 +0500

    contact view

---
+++ Islamia College Portfoio/Preview Content/Views/ContactUsView.swift
@@ -0,0 +1,18 @@
+//
+//  ContactUsView.swift
+//  Islamia College Portfoio
+//
+//  Created by Development on 18/06/2025.
+//
+
+import SwiftUI
+
+struct ContactUsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactUsView()
+}
