6fae4037f6dadde8d4807a4901554024b0def7da
Zaid Ali <zaidali786908@gmail.com>
2025-06-19 20:03:41 +0500

    changes

---
+++ Islamia College Portfoio/Preview Content/Models/EventsModel.swift
@@ -0,0 +1,8 @@
+//
+//  EventsModel.swift
+//  Islamia College Portfoio
+//
+//  Created by Development on 19/06/2025.
+//
+
+import Foundation
---
+++ Islamia College Portfoio/Preview Content/Views/DemoVideos.swift
@@ -0,0 +1,18 @@
+//
+//  DemoVideos.swift
+//  Islamia College Portfoio
+//
+//  Created by Development on 19/06/2025.
+//
+
+import SwiftUI
+
+struct DemoVideos: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DemoVideos()
+}
