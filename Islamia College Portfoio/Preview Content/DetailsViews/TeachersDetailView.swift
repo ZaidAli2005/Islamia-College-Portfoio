2eccd1d82ea36cdd915b5338a0e994b00a8dafc1
Zaid Ali <zaidali786908@gmail.com>
2025-06-20 12:51:09 +0500

    minor changes

---
+++ Islamia College Portfoio/Preview Content/DetailsViews/TeachersDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TeachersDetailView.swift
+//  Islamia College Portfoio
+//
+//  Created by Development on 20/06/2025.
+//
+
+import SwiftUI
+
+struct TeachersDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeachersDetailView()
+}
---
+++ Islamia College Portfoio/Preview Content/Models/TeachersModel.swift
@@ -0,0 +1,8 @@
+//
+//  TeachersModel.swift
+//  Islamia College Portfoio
+//
+//  Created by Development on 20/06/2025.
+//
+
+import Foundation
